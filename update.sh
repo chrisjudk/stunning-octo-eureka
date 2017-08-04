6935c0f7b7a23baa55139507af1326cf8d705619
Chrisjudk <chrisjudkins@live.com>
2017-08-04 18:17:27 -0400

    Added tstelnet to automatically send shutdown notification. Updated update.sh to point to current directory for tstelnet.sh.

--- update.sh
+++ update.sh
@@ -15,5 +15,5 @@ if [ $? -eq 0 ]
 then
   echo $NOW: Update complete, no restart required >> /var/local/log/update.log
 else
-  echo $NOW: Update complete, restart required >> /var/local/log/update.log && /usr/local/bin/teamspeak/tstelnet1.sh && /etc/init.d/teamspeak3 stop && /sbin/shutdown -r now
+  echo $NOW: Update complete, restart required >> /var/local/log/update.log && ./tstelnet.sh && /etc/init.d/teamspeak3 stop && /sbin/shutdown -r now
 fi
