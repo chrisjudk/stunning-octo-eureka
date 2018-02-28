e30a4d8cb1e297057a12b5fa3fe74be690d5979f
Chrisjudk <chrisjudkins@live.com>
2018-02-28 01:48:50 -0500

    Fix for dpkg config file error

--- update.sh
+++ update.sh
@@ -2,14 +2,16 @@
 NOW=$(date +'%Y-%m-%d')
 Telnetlogin=$(sed -n '2,2p; 3q' ./credentials)
 Telnetpassword=$(sed -n '4,4p; 5q' ./credentials)
+export DEBIAN_FRONTEND=noninteractive
+export DEBIAN_PRIORITY=critical
 echo 'apt-get update' >> /var/local/apt/$NOW.log
-apt-get update >> /var/local/apt/$NOW.log
+apt-get -y update >> /var/local/apt/$NOW.log
 echo 'apt-get dist-upgrade' >> /var/local/apt/$NOW.log
-apt-get dist-upgrade -y >> /var/local/apt/$NOW.log
+apt-get -y -o "Dpkg::Options::--force-confdef" -o "Dpkg::Options::=--force-confold" dist-upgrade >> /var/local/apt/$NOW.log
 echo 'apt-get autoclean' >> /var/local/apt/$NOW.log
-apt-get autoclean >> /var/local/apt/$NOW.log
+apt-get -y autoclean >> /var/local/apt/$NOW.log
 echo 'apt-get autoremove' >> /var/local/apt/$NOW.log
-apt-get autoremove -y >> /var/local/apt/$NOW.log
+apt-get -y autoremove >> /var/local/apt/$NOW.log
 #CHECKRESTART=$(/usr/sbin/checkrestart -v | grep -q 'Found 0 processes using old versions of upgraded file')
 #$CHECKRESTART
 /usr/sbin/checkrestart -v | grep -q 'Found 0 processes using old versions of upgraded file'
