49b803760ef1e812f74a74fb74a66cc01e96f890
Chrisjudk <chrisjudkins@live.com>
2018-05-08 02:07:51 -0400

    Moved apt log location

--- update.sh
+++ update.sh
@@ -4,14 +4,14 @@ Telnetlogin=$(sed -n '2,2p; 3q' ./credentials)
 Telnetpassword=$(sed -n '4,4p; 5q' ./credentials)
 export DEBIAN_FRONTEND=noninteractive
 export DEBIAN_PRIORITY=critical
-echo 'apt-get update' >> /var/local/apt/$NOW.log
-apt-get -y update >> /var/local/apt/$NOW.log
-echo 'apt-get dist-upgrade' >> /var/local/apt/$NOW.log
-apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade >> /var/local/apt/$NOW.log
-echo 'apt-get autoclean' >> /var/local/apt/$NOW.log
-apt-get -y autoclean >> /var/local/apt/$NOW.log
-echo 'apt-get autoremove' >> /var/local/apt/$NOW.log
-apt-get -y autoremove >> /var/local/apt/$NOW.log
+echo 'apt-get update' >> /var/local/log/apt/$NOW.log
+apt-get -y update >> /var/local/log/apt/$NOW.log
+echo 'apt-get dist-upgrade' >> /var/local/log/apt/$NOW.log
+apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade >> /var/local/log/apt/$NOW.log
+echo 'apt-get autoclean' >> /var/local/log/apt/$NOW.log
+apt-get -y autoclean >> /var/local/log/apt/$NOW.log
+echo 'apt-get autoremove' >> /var/local/log/apt/$NOW.log
+apt-get -y autoremove >> /var/local/log/apt/$NOW.log
 #CHECKRESTART=$(/usr/sbin/checkrestart -v | grep -q 'Found 0 processes using old versions of upgraded file')
 #$CHECKRESTART
 /usr/sbin/checkrestart -v | grep -q 'Found 0 processes using old versions of upgraded file'
