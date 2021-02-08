1f5a2f1c8afdcba8e3d80889edecf9e92feba788
Chris Judkins <chrisjudk@protonmail.com>
2021-02-08 13:50:06 -0500

    Removed Teamspeak stuff & added an error log file

--- update.sh
+++ update.sh
@@ -3,8 +3,8 @@ NOW=$(date +'%Y-%m-%d') # Get current date and put in YYYY-MM-DD format
 cd "${0%/*}" # Changed directory to the directory of this file
 
 # Read telnet credentials from file (./credentials)
-telnetLogin=$(sed -n '2,2p; 3q' ./credentials)
-telnetPassword=$(sed -n '4,4p; 5q' ./credentials)
+#telnetLogin=$(sed -n '2,2p; 3q' ./credentials)
+#telnetPassword=$(sed -n '4,4p; 5q' ./credentials)
 
 # Set specific variables used for apt-get
 export DEBIAN_FRONTEND=noninteractive
@@ -12,14 +12,17 @@ export DEBIAN_PRIORITY=critical
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
 #Updates
+echo "$NOW: Begin" >> /var/local/log/error.log
+echo 'NOTE: All apt errors will be forwarded to /var/local/log/error.log' >> /var/local/log/apt/$NOW.log
 echo '----------[apt-get update]--------------------' >> /var/local/log/apt/$NOW.log
-apt-get -y update >> /var/local/log/apt/$NOW.log # Get updates and write output to log file with current date as title
+apt-get -y update >> /var/local/log/apt/$NOW.log 2>/var/local/log/error.log # Get updates and write output to log file with current date as title
 echo '----------[apt-get dist-upgrade]--------------------' >> /var/local/log/apt/$NOW.log
-apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade >> /var/local/log/apt/$NOW.log # Install updates and write output to log file
+apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade > /var/local/log/apt/$NOW.log 2>/var/local/log/error.log# Install updates and write output to log file
 echo '----------[apt-get autoclean]--------------------' >> /var/local/log/apt/$NOW.log
-apt-get -y autoclean >> /var/local/log/apt/$NOW.log # Autoclean and output to log
+apt-get -y autoclean > /var/local/log/apt/$NOW.log 2>/var/local/log/error.log # Autoclean and output to log
 echo '----------[apt-get autoremove]--------------------' >> /var/local/log/apt/$NOW.log
-apt-get -y autoremove >> /var/local/log/apt/$NOW.log # Autoremove and output to log
+apt-get -y autoremove > /var/local/log/apt/$NOW.log 2>/var/local/log/error.log # Autoremove and output to log
+echo "$NOW: End\n"  >> /var/local/log/error.log
 
 #Check if Restart is required
 /usr/sbin/checkrestart -v | grep -q 'Found 0 processes using old versions of upgraded file'
@@ -28,7 +31,7 @@ then
   echo $NOW: Update complete, no restart required >> /var/local/log/update.log
 else
   echo $NOW: Update complete, restart required >> /var/local/log/update.log
-  ./tstelnet.sh $telnetLogin $telnetPassword # Run telnet to send global message to TS server letting everyone know that the server is about to shutdown
-  sudo -u ts3server /home/ts3server/ts3server stop # Stop the teamspeak server
+  #./tstelnet.sh $telnetLogin $telnetPassword # Run telnet to send global message to TS server letting everyone know that the server is about to shutdown
+  # sudo -u ts3server /home/ts3server/ts3server stop # Stop the teamspeak server
   /sbin/shutdown -r now # Shutdown system
 fi
