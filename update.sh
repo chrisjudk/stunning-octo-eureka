9d4cf6179052daf39758a6f627ec9731eadead8f
chrisjudk <chrisjudk@protonmail.com>
2021-04-24 04:24:26 -0400

    Removed teamspeak shutdown code and related files

--- update.sh
+++ update.sh
@@ -2,10 +2,6 @@
 NOW=$(date +'%Y-%m-%d') # Get current date and put in YYYY-MM-DD format
 cd "${0%/*}" # Changed directory to the directory of this file
 
-# Read telnet credentials from file (./credentials)
-#telnetLogin=$(sed -n '2,2p; 3q' ./credentials)
-#telnetPassword=$(sed -n '4,4p; 5q' ./credentials)
-
 # Set specific variables used for apt-get
 export DEBIAN_FRONTEND=noninteractive
 export DEBIAN_PRIORITY=critical
@@ -31,7 +27,5 @@ then
   echo $NOW: Update complete, no restart required > >(tee -a /var/local/log/update.log)
 else
   echo $NOW: Update complete, restart required > >(tee -a /var/local/log/update.log)
-  #./tstelnet.sh $telnetLogin $telnetPassword # Run telnet to send global message to TS server letting everyone know that the server is about to shutdown
-  # sudo -u ts3server /home/ts3server/ts3server stop # Stop the teamspeak server
   /sbin/shutdown -r now # Shutdown system
 fi
